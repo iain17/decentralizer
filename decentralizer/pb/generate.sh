b3347a703d75bd952c5e8f231b1a8c119decbff0
Iain Munro <iain17@me.com>
2017-03-01 01:47:31 +0100

    moved things around. let us use grpc for the clients. So I don't have to end up writing a client for each language.

--- decentralizer/pb/generate.sh
+++ decentralizer/pb/generate.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+
+#Create go client/server files
 protoc *.proto --go_out=plugins=grpc:.
+
+#Create client
+#protoc *.proto --go_out=plugins=grpc:.
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/iain17/decentralizer/cmd"
+
+func main() {
+	cmd.Execute()
+}
