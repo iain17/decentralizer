58182d498c5fe7076b0d377e107270ec25c84ab7
Iain Munro <iain17@me.com>
2017-12-13 20:38:17 +0100

    Fixing CI.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-gx install
+$(GOPATH)/bin/gx install
 #Patch a stupid fucking problem because of gx and the way ipfs does deps: debug/requests problem
 find $(GOPATH)/src/gx/ -name 'trace.go' -exec sed -i '.bak' -e 's/requests"/requestss"/g' {} \;
 find $(GOPATH)/src/gx/ -name 'trace.go' -exec sed -i '.bak' -e 's/events"/eventss"/g' {} \;
 find $(GOPATH)/src/gx/ -name '*.bak' -type f -exec rm -f {} +
-dep ensure
+$(GOPATH)/bin/dep ensure
