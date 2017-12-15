997847cdf62bc41f5cad38bed725ca42b72d5fbb
Iain Munro <iain17@me.com>
2017-12-15 03:57:28 +0100

    fixed deadlock with session deletion. too many or timed out.

--- app/sessionstore/lruTTL.go
+++ app/sessionstore/lruTTL.go
@@ -3,14 +3,17 @@ package sessionstore
 import "github.com/iain17/logger"
 
 func (s *Store) onEvicted(key interface{}, value interface{}) {
-	println("damn")
-	if sSessionId, ok := key.(uint64); ok {
-		println("k")
-		err := s.Remove(sSessionId)
-		if err != nil {
-			logger.Warningf("Could not delete session id: %s", sSessionId)
+	//In go routine. Because of deadlock caused when evicted is called, the insert function higher up the call chain has just locked the mutex.
+	go func() {
+		if sSessionId, ok := key.(uint64); ok {
+			err := s.Remove(sSessionId)
+			if err != nil {
+				logger.Warningf("Could not delete session id: %s", sSessionId)
+			} else {
+				logger.Infof("Deleted session id: %s", sSessionId)
+			}
 		} else {
-			logger.Infof("Deleted session id: %s", sSessionId)
+			logger.Warningf("Could not delete session id...", sSessionId)
 		}
-	}
+	}()
 }
--- app/sessionstore/store_test.go
+++ app/sessionstore/store_test.go
@@ -32,51 +32,51 @@ func TestStore_FindByPeerId(t *testing.T) {
 	assert.Equal(t, sessions[0].PId, "test1")
 }
 
-//func TestStore_Expire(t *testing.T) {
-//	store, err := New(10, 1 * time.Second)
-//	assert.NoError(t, err)
-//	_, err = store.Insert(&pb.Session {
-//		DnId: 1,
-//		PId: "test1",
-//		Details: map[string]string{
-//			"hey": "ho",
-//		},
-//	})
-//	assert.NoError(t, err)
-//
-//	sessions, err := store.FindAll()
-//	assert.NoError(t, err)
-//	assert.Equal(t, 1, len(sessions))
-//	time.Sleep(10 * time.Second)
-//	sessions, err = store.FindAll()
-//	assert.NoError(t, err)
-//	assert.Equal(t, 0, len(sessions))
-//}
-//
-//func TestStore_Limit(t *testing.T) {
-//	store, err := New(1, 2 * time.Second)
-//	assert.NoError(t, err)
-//	_, err = store.Insert(&pb.Session {
-//		DnId: 1,
-//		PId: "test1",
-//		Details: map[string]string{
-//			"hey": "ho",
-//		},
-//	})
-//	assert.NoError(t, err)
-//	_, err = store.Insert(&pb.Session {
-//		DnId: 2,
-//		PId: "test2",
-//		Details: map[string]string{
-//			"hey": "ho",
-//		},
-//	})
-//	assert.NoError(t, err)
-//
-//	sessions, err := store.FindAll()
-//	assert.NoError(t, err)
-//	assert.True(t, len(sessions) == 1)
-//}
+func TestStore_Expire(t *testing.T) {
+	store, err := New(10, 1 * time.Second)
+	assert.NoError(t, err)
+	_, err = store.Insert(&pb.Session {
+		DnId: 1,
+		PId: "test1",
+		Details: map[string]string{
+			"hey": "ho",
+		},
+	})
+	assert.NoError(t, err)
+
+	sessions, err := store.FindAll()
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(sessions))
+	time.Sleep(2 * time.Second)
+	sessions, err = store.FindAll()
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(sessions))
+}
+
+func TestStore_Limit(t *testing.T) {
+	store, err := New(1, 2 * time.Hour)
+	assert.NoError(t, err)
+	_, err = store.Insert(&pb.Session {
+		DnId: 1,
+		PId: "test1",
+		Details: map[string]string{
+			"hey": "ho",
+		},
+	})
+	assert.NoError(t, err)
+	_, err = store.Insert(&pb.Session {
+		DnId: 2,
+		PId: "test2",
+		Details: map[string]string{
+			"hey": "ho",
+		},
+	})
+	assert.NoError(t, err)
+	time.Sleep(1 * time.Second)
+	sessions, err := store.FindAll()
+	assert.NoError(t, err)
+	assert.True(t, len(sessions) == 1)
+}
 
 func TestStore_FindByDetails(t *testing.T) {
 	store, err := New(10, 1 * time.Hour)
